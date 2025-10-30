a479c7cb5489754b1bdd100f4a1852d6fef2835e
barmalat <barmalat@yahoo.com>
2025-10-30 16:56:20 +0100

    +streams exercises

--- streams-excercise/src/homework/Exercises.java
+++ streams-excercise/src/homework/Exercises.java
@@ -1,7 +1,7 @@
 package homework;
 
-import homework.generator.CompanyGenerator;
 import homework.generator.HoldingGenerator;
+import homework.generator.UserGenerator;
 import homework.model.*;
 import homework.model.Currency;
 
@@ -18,6 +18,7 @@ import java.util.stream.Stream;
 public class Exercises {
 
     private static final List<Holding> holdings = new HoldingGenerator().generate();
+    private static final List<User> users = new UserGenerator().generate();
 
     public static void main(String[] args) {
         System.out.println("1: " + getHoldingsWhereAreCompanies());
@@ -27,11 +28,24 @@ public class Exercises {
         System.out.println("5: " + getAllUserAmount());
         System.out.println("6: " + getAllCompaniesNamesAsLinkedList());
         //System.out.println("7: " + getAccountAmountInPLN());
-        System.out.println("8: " +getUsersForPredicate(user -> user.getSex()==Sex.OTHER));
+        System.out.println("8: " + getUsersForPredicate(user -> user.getSex() == Sex.OTHER));
+        System.out.println("9: ");
+        System.out.println("10: " + getRichestWoman()
+                .map(user -> String.valueOf(user.getFirstName() + " " + user.getLastName())));
+        System.out.println("11: " + getFirstNCompany(4));
+        System.out.println("12: " + getUserPerCompany());
+        String getUserToString = getUser(user -> user.getAge() > 10).getFirstName();
+        System.out.println("13: " + getUserToString);
+        System.out.println("14: " + getAccoutStream());
+        System.out.println("15: " + getUserNames());
+        System.out.print("16: ");
+        showAllUser();
+        System.out.println("17: " + getCurenciesSet());
 
     }
 
     /**
+     * 1
      * Napisz metodę, która zwróci liczbę holdingów, w których jest przynajmniej jedna firma.
      */
     public static long getHoldingsWhereAreCompanies() {
@@ -41,6 +55,7 @@ public class Exercises {
     }
 
     /**
+     * 2
      * Napisz metodę, która zwróci nazwy wszystkich holdingów pisane z wielkiej litery w formie listy.
      */
     public static List<String> getHoldingNames() {
@@ -51,6 +66,7 @@ public class Exercises {
     }
 
     /**
+     * 3
      * Zwraca nazwy wszystkich holdingów sklejone w jeden string i posortowane.
      * String ma postać: (Coca-Cola, Nestle, Pepsico)
      */
@@ -62,6 +78,7 @@ public class Exercises {
     }
 
     /**
+     * 4
      * Zwraca liczbę firm we wszystkich holdingach.
      */
     public static long getCompaniesAmount() {
@@ -72,6 +89,7 @@ public class Exercises {
 
 
     /**
+     * 5
      * Zwraca liczbę wszystkich pracowników we wszystkich firmach.
      */
     public static long getAllUserAmount() {
@@ -82,6 +100,7 @@ public class Exercises {
     }
 
     /**
+     * 6
      * Zwraca listę wszystkich firm jako listę, której implementacja to LinkedList. Obiektów nie przepisujemy
      * po zakończeniu działania strumienia.
      */
@@ -93,6 +112,7 @@ public class Exercises {
     }
 
     /**
+     * 7
      * Przelicza kwotę na rachunku na złotówki za pomocą kursu określonego w enum Currency.
      */
     public static BigDecimal getAccountAmountInPLN(Account account) {
@@ -103,6 +123,7 @@ public class Exercises {
     }
 
     /**
+     * 8
      * Zwraca imiona użytkowników w formie zbioru, którzy spełniają podany warunek.
      */
     public static Set<String> getUsersForPredicate(final Predicate<User> userPredicate) {
@@ -115,71 +136,120 @@ public class Exercises {
     }
 
     /**
+     * 9
      * Dla każdej firmy uruchamia przekazaną metodę.
      */
     public static void executeForEachCompany(Consumer<Company> consumer) {
     }
 
     /**
+     * 10
      * Wyszukuje najbogatsza kobietę i zwraca ją. Metoda musi uzwględniać to że rachunki są w różnych walutach.
      */
     //pomoc w rozwiązaniu problemu w zadaniu: https://stackoverflow.com/a/55052733/9360524
     public static Optional<User> getRichestWoman() {
-        return Optional.empty();
+        return users.stream()
+                .filter(user -> user.getSex() == Sex.WOMAN).max(Comparator.comparing(user -> getUserAmountInPLN(user)));
+
     }
 
     private static BigDecimal getUserAmountInPLN(final User user) {
-        return null;
+        return users.stream()
+                .filter(x -> x.equals(user))
+                .flatMap(x -> x.getAccounts().stream())
+                .map(account -> getAccountAmountInPLN(account))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     /**
+     * 11
      * Zwraca nazwy pierwszych N firm. Kolejność nie ma znaczenia.
      */
     private static Set<String> getFirstNCompany(final int n) {
-        return null;
+        return holdings
+                .stream()
+                .flatMap(holding -> holding.getCompanies().stream())
+                .map(company -> String.valueOf(company.getName()))
+                .limit(n)
+                .collect(Collectors.toSet());
     }
 
     /**
+     * 12
      * Zwraca mapę firm, gdzie kluczem jest jej nazwa a wartością lista pracowników.
      */
     public static Map<String, List<User>> getUserPerCompany() {
-        return null;
+        return holdings
+                .stream()
+                .flatMap(holding -> holding.getCompanies().stream())
+                .collect(Collectors.toMap(company -> String.valueOf(company.getName()), company -> company
+                        .getUsers()
+                        .stream()
+                        //.map(user -> (user.getFirstName() + " " + user.getLastName()))
+                        .toList()
+                ));
     }
 
     /**
+     * 13
      * Zwraca pierwszego z brzegu użytkownika dla podanego warunku. W przypadku kiedy nie znajdzie użytkownika, wyrzuca
      * wyjątek IllegalArgumentException.
      */
     public static User getUser(final Predicate<User> predicate) {
-        return null;
+        return users
+                .stream()
+                .filter(predicate)
+                .limit(1)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Nie ma użytkownika spełniającego zadany filtr"));
     }
 
     /**
+     * 14
      * Zwraca mapę rachunków, gdzie kluczem jest numer rachunku, a wartością ten rachunek.
      */
     public static Map<String, Account> createAccountsMap() {
-        return null;
+        return getAccoutStream()
+                .collect(Collectors.toMap(Account::getNumber, account -> account));
     }
 
     /**
+     * 15
      * Zwraca listę wszystkich imion w postaci Stringa, gdzie imiona oddzielone są spacją i nie zawierają powtórzeń.
      */
     public static String getUserNames() {
-        return null;
+        return users
+                .stream()
+                .map(User::getFirstName)
+                .sorted()
+                .distinct()
+                .collect(Collectors.joining(" "));
     }
 
     /**
+     * 16
      * Metoda wypisuje na ekranie wszystkich użytkowników (imię, nazwisko) posortowanych od z do a.
      * Zosia Psikuta, Zenon Kucowski, Zenek Jawowy ... Alfred Pasibrzuch, Adam Wojcik
      */
     public static void showAllUser() {
+        List<String> x = users
+                .stream()
+                .map(user -> user.getFirstName() + " " + user.getLastName())
+                .sorted(Comparator.reverseOrder())
+                .toList();
+        System.out.println(x);
     }
 
     /**
+     * 17
      * Zwraca zbiór walut w jakich są rachunki.
      */
     public static Set<Currency> getCurenciesSet() {
-        return null;
+        return users
+                .stream()
+                .flatMap(user -> user.getAccounts().stream())
+                .map(Account::getCurrency)
+                .collect(Collectors.toSet());
     }
 
     /**
@@ -205,5 +275,4 @@ public class Exercises {
         return getUserStream()
                 .flatMap(user -> user.getAccounts().stream());
     }
-
 }
